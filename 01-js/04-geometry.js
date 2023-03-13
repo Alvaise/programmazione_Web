76e8ab3c8c912489d60d846f7cd1103ce7b46bf5
Alvaise <79141448+Alvaise@users.noreply.github.com>
2023-03-13 17:16:20 +0100

    a

--- 01-js/04-geometry.js
+++ 01-js/04-geometry.js
@@ -5,4 +5,4 @@ let circumference = (2*Math.PI)*r;
 let circumference2 = Math.PI*diam;
 let area = Math.PI * (Math.pow(2,r));
 console.log("The circumference is "+circumference);
-console.log(area);
+console.log(typeof area);
