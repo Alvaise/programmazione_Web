ddfecc41f57d33f881843c9ad65c8484365fae47
Alvaise <79141448+Alvaise@users.noreply.github.com>
2023-03-17 17:47:39 +0100

    a

--- 01-js/01-fortune-teller.js
+++ 01-js/01-fortune-teller.js
@@ -3,4 +3,4 @@ let partName = "Francesco";
 let loc = "Casal Pusterlengo";
 let jobT = "President of the universe";
 
-console.log("You will be a "+jobT+" in "+loc+",and married to "+partName+" with "+numChildren+" kids");
+console.log("You will be a " + jobT + " in " + loc + ",and married to " + partName + " with " + numChildren + " kids");
---
+++ 02-functions/01-variable-scope.js
@@ -0,0 +1,29 @@
+function addNumbers(num1, num2) {
+    let result = num1 + num2;
+    return result;
+}
+let i;// global scope
+for(i=0;i<20;i++){
+    let num1=i; 
+    let num2=i+1;
+    let r= addNumbers(num1,num2);
+    console.log(r+"a");
+    num1++; 
+    num2++;
+}
+for(i=0;i<20;i++){
+    let num1=i; 
+    let num2=i+1;
+    let r= addNumbers(num1,num2);
+    console.log(r+"b");
+    num1++; 
+    num2++;
+}
+for(i=0;i<20;i++){
+    let num1=i; 
+    let num2=i+1;
+    let r= addNumbers(num1,num2);
+    console.log(r+"c");
+    num1++; 
+    num2++;
+}
