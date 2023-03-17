fcee3030ebb47205b552106930e2f29270aaf2c5
Alvaise <79141448+Alvaise@users.noreply.github.com>
2023-03-17 12:50:53 +0100

    a

--- 01-js/05-temperature.js
+++ 01-js/05-temperature.js
@@ -1,3 +1,6 @@
 let gradiC = 25;
 let gradiF = (gradiC * 9/5) + 32;
 console.log(gradiC + ' Celsius sono '+gradiF+' fahrenheit'); 
+let gF= 77;
+let gC= (gF-32)*5/9;
+console.log(gF+' F sono '+gC+' gradi celsius');
