2898e472d4efaf9e7fc31748a9638e2dd4a65970
Alvaise <79141448+Alvaise@users.noreply.github.com>
2023-03-17 17:56:33 +0100

    a

--- 02-functions/01-variable-scope.js
+++ 02-functions/01-variable-scope.js
@@ -2,6 +2,14 @@ function addNumbers(num1, num2) {
     let result = num1 + num2;
     return result;
 }
+function addNumbers(num1, num2) { // metti arrow function
+    let result = num1 + num2;
+    return result;
+}
+function addNumbers(num1, num2) { // metti arrow function
+    let result = num1 + num2;
+    return result;
+}
 let i;// global scope
 for(i=0;i<20;i++){
     let num1=i; 
