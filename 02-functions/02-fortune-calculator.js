9075719fbc5efd641e66aa1d710abe7c775dfab7
Alvaise <79141448+Alvaise@users.noreply.github.com>
2023-03-19 15:12:11 +0100

    a

---
+++ 02-functions/02-fortune-calculator.js
@@ -0,0 +1,7 @@
+function tellFortune(numKids, partName, geoLoc, job) {
+    console.log('You will be a ' + job + ' in ' + geoLoc + ',and married to ' + partName + ' with ' + numKids + ' Kids');
+}
+
+tellFortune(1, 'Alejandro', 'Jupiter', 'Assistant to the regional manager');
+tellFortune(2, 'Federica', 'Busto Garolfo', 'Cop');
+tellFortune(3, 'Consuelo', 'Cormano', 'Driver of Subaru');
