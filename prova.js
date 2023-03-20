68ebc8a7bb682db1100380ccc1d99b0d0ed2c17d
Alvaise <79141448+Alvaise@users.noreply.github.com>
2023-03-20 17:26:18 +0100

    a

--- prova.js
+++ prova.js
@@ -1,1 +1,26 @@
-console.log('asdsdasdas');
+ /* function fibonacci(n) {
+    if (n<2) {
+        return n;
+    }
+    return fibonacci(n-1) + fibonacci(n-2);
+}
+*/
+// let catsRule = true;
+// if (catsRule) {
+//     console.log('Yay cats!');
+// }
+// console.log(typeof(catsRule));
+let firstName = 0;
+if(firstName){
+    console.log('Hello, '+firstName);
+}
+//console.log(typeof(firstName));
+// let secondName;
+// if(secondName){
+//     console.log('Your name is '+ secondName);
+// }
+//console.log(typeof(secondName));
+let points;
+if (points) {
+    console.log('You Have '+ points + ' points');
+}
