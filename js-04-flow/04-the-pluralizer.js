c3de93c7eb1ecaa4c1bdce5a8a06063342789bac
Alvaise <79141448+Alvaise@users.noreply.github.com>
2023-03-27 15:14:19 +0200

    con funzione upgradata

--- js-04-flow/04-the-pluralizer.js
+++ js-04-flow/04-the-pluralizer.js
@@ -12,20 +12,18 @@
  * ex. 2 cat ==> 2 cats
  * @param noun 
  * @param number 
- * 
+ * @return noun at the plural form
  */
-function pluralize(noun, number) {
+function pluralize(noun, number) { // funzione upgradata 
     if (number > 1) {
         noun += 's';
-        return noun
-    } else{
-        return noun;
-    }
+    } 
+    return noun; 
 }
 
 let noun ='cat';
 let number = 2;
 console.log(number + ' '+ pluralize(noun,number));
- noun ='cat';
- number = 1;
+noun ='cat';
+number = 1;
 console.log(number + ' '+ pluralize(noun,number));
