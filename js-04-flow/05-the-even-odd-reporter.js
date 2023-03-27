fc9af124ccc663266454f8b08255acccf2ce18bf
Alvaise <79141448+Alvaise@users.noreply.github.com>
2023-03-27 14:03:51 +0200

    a

---
+++ js-04-flow/05-the-even-odd-reporter.js
@@ -0,0 +1,15 @@
+/**
+* @file: 05-the-even-odd-reporter.js
+* @author: Alvise Spadea
+* Exercise on the flow of javascript
+*
+*  Uses a for loop to print the even odd number
+* 
+*/
+for (let i= 0; i<= 20; i++) {
+    if (i%2==0) {
+        console.log(i+' is even');
+    } else {
+        console.log(i+' is odd');
+    }  
+}
