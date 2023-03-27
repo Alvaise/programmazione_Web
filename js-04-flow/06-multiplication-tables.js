1c047f78852401bd182f07f79e695191a4112af8
Alvaise <79141448+Alvaise@users.noreply.github.com>
2023-03-27 18:00:17 +0200

    a

---
+++ js-04-flow/06-multiplication-tables.js
@@ -0,0 +1,14 @@
+/**
+* @file: 05-the-even-odd-reporter.js
+* @author: Alvise Spadea
+* Exercise on the flow of javascript
+*
+*  Uses a for loop to multiply a numer by 9 
+* 
+*/
+for (let i = 0; i <= 10; i++) {
+    let r = i*9;
+    console.log(i+"*9= "+r);
+}
+
+//nested loop
--- prova.js
+++ prova.js
@@ -10,17 +10,62 @@
 //     console.log('Yay cats!');
 // }
 // console.log(typeof(catsRule));
-let firstName = 0;
-if(firstName){
-    console.log('Hello, '+firstName);
-}
+// let firstName = 0;
+// if(firstName){
+//     console.log('Hello, '+firstName);
+// }
 //console.log(typeof(firstName));
 // let secondName;
 // if(secondName){
 //     console.log('Your name is '+ secondName);
 // }
 //console.log(typeof(secondName));
-let points;
-if (points) {
-    console.log('You Have '+ points + ' points');
-}
+// let points;
+// if (points) {
+//     console.log('You Have '+ points + ' points');
+// }
+
+// length
+// const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// let alphabetLength = alphabet.length;
+// console.log(alphabetLength);
+//  charAt()
+let greeting = "HELLO WORLD";
+let result = greeting.charAt(6);
+console.log(result);
+// indexOf()
+// let statement = "Hello world, welcome to the universe.";
+// let wordPosition = statement.indexOf("welcome");
+// console.log(wordPosition);
+
+// property access
+// const alphabetLowercase = "abcdefghijklmnopqrstuvwxyz";
+// console.log(alphabetLowercase[2]); // 'a'
+// console.log(alphabetLowercase.charAt(1));
+// console.log(alphabetLowercase.length);
+// charAt()
+// let greeting = "HELLO WORLD";
+// let result = greeting.charAt(0);
+// console.log(result);
+
+// let alphabetLowercase = "abcdefghijklmnopqrstuvwxyz?'èéèòçò@°à#";
+//  for (let i = 0; i < alphabetLowercase.length; i++) {
+    
+   console.log(alphabetLowercase.charCodeAt(i));
+//  }
+
+//console.log(/l/.test("The best things in life are free!"));
+
+/* const name = 'james';
+const age = 25;
+// interpolate variable bindings
+console.log(`My name is ${name} 
+I am ${age + 10} 
+years old (lie)`);
+// without using template strings
+ */
+
+let name = 'james';
+let age = 25;
+// without using template strings
+console.log('My name is '.concat(name, ' I am').concat(age + 10, ' years old (lie)'));
