f2f354f904e5323ce6c45eecabedfae6ed936596
Alvaise <79141448+Alvaise@users.noreply.github.com>
2023-03-28 08:44:00 +0200

    a

--- js-04-flow/06-multiplication-tables.js
+++ js-04-flow/06-multiplication-tables.js
@@ -14,6 +14,7 @@ for (let i = 0; i <= 10; i++) {
 //nested loop
 for (let i= 1; i<=10; i++) {
     for(i2=0;i2<10;i2++){
-        
+        let r = i2 * i;
+        console.log(i2+'*'+i+'='+r);
     }  
 }
