131df220e079a0acfc8061e9b131393b6fc1ce86
Alvaise <79141448+Alvaise@users.noreply.github.com>
2023-03-31 22:47:57 +0200

    a

--- js-05-strings/03-palindrome.js
+++ js-05-strings/03-palindrome.js
@@ -2,14 +2,54 @@
  * @file: 03-palindrome.js
  * @author: Alvise Spadea
  * Exercise on Strings in Javascript
+ * 
  * Using your reverse() function from the previous exercise, write a simple
-function to check if a string is a palindrome.
-● A palindrome is a word that reads the same backwards as forwards. For
-example, the word "madam" is a palindrome.
-● Write a JavaScript function called isPalindrome which has one parameter, a
-string, and which returns true if that string is a palindrome, else false.
-● For example, the call isPalindrome("madam") should return true, while
-isPalindrome("madame") should return false.
-● Bonus: Try to write the same function without using the reverse() function
+ * function to check if a string is a palindrome.
+ * A palindrome is a word that reads the same backwards as forwards. For
+ * example, the word "madam" is a palindrome.
+ * Write a JavaScript function called isPalindrome which has one parameter, a
+ * string, and which returns true if that string is a palindrome, else false.
+ * For example, the call isPalindrome("madam") should return true, while
+ * isPalindrome("madame") should return false.
+ * Bonus: Try to write the same function without using the reverse() function
  *  
  */
+
+/**
+ * function that take a string and return the same string but in reverse order
+ * @param str a String
+ * @returns the string in reverse
+ */
+function printReverse(str) {
+    let rts='';
+    for(i=str.length;i>=0;i--){
+        
+        rts= rts+str.charAt(i); 
+    }
+    return rts;
+}
+/**
+ * function that check if a string is a palindrome, casing don't matter but the white spaces do
+ * so it works only on single word. For phrases it won't work.
+ * @param {*} str 
+ * @returns true is the word is a palidrome or false
+ */
+function isPalindrome(str) {
+    str=str.toUpperCase();
+    let rts=printReverse(str);
+    if (str==rts) {
+        return true;
+    }
+    return false; 
+}
+
+/**
+ * the is palidrome functions but without using the printReverse function()
+ */
+function isPalindromeNoRev(str) {
+    
+}
+
+let str='MadaMe';
+
+console.log(isPalindrome(str));
--- js-05-strings/04-capital.js
+++ js-05-strings/04-capital.js
@@ -3,8 +3,27 @@
  * @author: Alvise Spadea
  * Exercise on Strings in Javascript
  * 
- * Write a JavaScript function called printReverse which has one parameter, a
- * string, and which prints that string in reverse.
- * For example, the call printReverse("foobar") should result in "raboof" being
- * displayed. 
+ * Write a JavaScript function called capital which has one parameter, a string,
+ * and which returns that string with the first letter capitalized.
+ * For example, the call capital("hello world") should return the string "Hello
+ * world".
+ * Bonus: modify the function so that it capitalizes each word.
+ * capital2("my name is john") should return the string "My Name Is John"
  */
+
+/**
+ * function that capitalize the first letter 
+ * @param str a string
+ * @returns c the string with the first letter capitalized
+ */
+function capital(str) {
+    let c = str.charAt(0).toUpperCase() + str.slice(1);
+
+    return c;
+}
+
+
+
+let a = 'hello world';
+
+console.log(capital(a)); 
