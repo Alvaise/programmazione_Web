505f91024f681daa5e3dae21dab6bf5ed657d09b
Alvaise <79141448+Alvaise@users.noreply.github.com>
2023-04-02 18:26:06 +0200

    a

--- js-05-strings/06-mixup.js
+++ js-05-strings/06-mixup.js
@@ -3,8 +3,29 @@
  * @author: Alvise Spadea
  * Exercise on Strings in Javascript
  * 
- * Write a JavaScript function called printReverse which has one parameter, a
- * string, and which prints that string in reverse.
- * For example, the call printReverse("foobar") should result in "raboof" being
- * displayed. 
+ * Create a function called mixUp.
+ * It should take in two strings, and return the concatenation of the two strings
+ * (separated by a space) slicing out and swapping the first 2 characters of
+ * each.
+ * You can assume that the strings are at least 2 characters long. 
  */
+
+/**
+ * function that swaps the first two character of two strings
+ * @param str1
+ * @param str2
+ * @returns str1+str2
+ */
+function mixUp(str1,str2) {
+    a=str2.slice(0,2);
+    b=str1.slice(0,2);
+    str1 = a + str1.slice(2);
+    str2 = b + str2.slice(2);
+    return str1+' '+str2;
+}
+
+
+console.log('dog dinner');
+console.log(mixUp('dog','dinner'));
+console.log('mix pod');
+console.log(mixUp('mix','pod'));
