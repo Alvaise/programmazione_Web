6236ac7e3b6711264668a4de4f87742767839078
Alvaise <79141448+Alvaise@users.noreply.github.com>
2023-04-03 14:13:03 +0200

    a

--- js-05-strings/09-not-bad.js
+++ js-05-strings/09-not-bad.js
@@ -3,8 +3,14 @@
  * @author: Alvise Spadea
  * Exercise on Strings in Javascript
  * 
- * Write a JavaScript function called printReverse which has one parameter, a
- * string, and which prints that string in reverse.
- * For example, the call printReverse("foobar") should result in "raboof" being
- * displayed. 
+ * Create a function called notBad that takes a single argument, a string.
+ * It should find the first appearance of the substring 'not' and 'bad'.
+ * If the 'bad' follows the 'not', then it should replace the whole 'not'...'bad'
+ * substring with 'good' and return the result.
+ * If it doesn't find 'not' and 'bad' in the right sequence (or at all), just return
+ * the original sentence.
  */
+
+function notBad(str) {
+    
+}
