4f7e443cbceec40afacdeb3865c9e75b0435e3d5
Alvaise <79141448+Alvaise@users.noreply.github.com>
2023-04-03 10:35:44 +0200

    a

--- js-05-strings/08-verbing.js
+++ js-05-strings/08-verbing.js
@@ -3,8 +3,23 @@
  * @author: Alvise Spadea
  * Exercise on Strings in Javascript
  * 
- * Write a JavaScript function called printReverse which has one parameter, a
- * string, and which prints that string in reverse.
- * For example, the call printReverse("foobar") should result in "raboof" being
- * displayed. 
+ * Create a function called verbing.
+ * It should take a single argument, a string. If its length is at least 3, it should
+ * add 'ing' to its end, unless it already ends in 'ing', in which case it should add
+ * 'ly' instead.
+ * If the string length is less than 3, it should leave it unchanged.
  */
+
+function verbing(str) {
+    let strE=str.slice(-3);
+    if (str.length>=3 && strE!='ing') {
+        return str +'ing';
+    } else if(str.length>=3 && strE=='ing') {
+        return str+'ly';
+    } 
+    return str;
+}
+
+console.log(verbing('swim')); 
+console.log(verbing('swimming')); 
+console.log(verbing('go')); 
