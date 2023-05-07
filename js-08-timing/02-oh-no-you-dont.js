3546bd651eb187f92d2997865b7bdf2b3a7abbd4
Alvaise <79141448+Alvaise@users.noreply.github.com>
2023-05-07 23:41:11 +0200

    a

---
+++ js-08-timing/02-oh-no-you-dont.js
@@ -0,0 +1,12 @@
+/**
+ * @file: 02-oh-no-you-dont.js
+ * @author: Alvise Spadea
+ * Exercise on timing in Javascript
+ * 
+ * Write a function “useful” that does something useful in Javascript.
+ * Schedule it to run after 10 seconds.
+ * Write another function that cancels the scheduling of the first function.
+ * Use the second function to cancel the first one after 5 seconds and output
+ * ‘function cancelled’ to the console.
+ */
+
