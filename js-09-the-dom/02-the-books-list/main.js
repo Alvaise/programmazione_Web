b9b7332f815bedba4ebf00659c0230e65cb1f3af
Alvaise <79141448+Alvaise@users.noreply.github.com>
2023-05-21 21:24:42 +0200

    a

--- js-09-the-dom/02-the-books-list/main.js
+++ js-09-the-dom/02-the-books-list/main.js
@@ -3,7 +3,7 @@ let books = [
         title: 'The Design of EveryDay Things',
         author: 'Don Norman',
         alreadyRead: false
-    }, 
+    },
     {
         title: 'The Most Human Human',
         author: 'Brian Christian',
@@ -23,18 +23,42 @@ let books = [
 
 let body = document.body;
 let head = document.head;
-let i =0;
+let i = 0;
 let ul = document.createElement('ul');
 body.appendChild(ul);
+const imagesURL = [
+    'https://m.media-amazon.com/images/I/416Hql52NCL.jpg',
+    'https://m.media-amazon.com/images/I/71HMyqG6MRL.jpg',
+    'https://m.media-amazon.com/images/I/81xqnfgEVRS.jpg',
+    'https://m.media-amazon.com/images/I/81Ipdy263NL.jpg'
+];
 books.forEach(element => {
     let li = document.createElement('li');
     let title = document.createElement('h2');
     let author = document.createElement('h3');
     title.textContent = books[i].title;
     author.textContent = books[i].author;
-    i++;
     li.appendChild(title);
     li.appendChild(author);
     ul.appendChild(li);
-    
+    books[i].url = imagesURL[i];
+    let bookCover = document.createElement('img');
+    bookCover.alt = 'cover-'+ books[i].title;
+    bookCover.src = imagesURL[i];
+    bookCover.width = 200;
+    li.appendChild(bookCover);
+    if (books[i].alreadyRead) {
+        li.style.backgroundColor = 'green';
+        let gifNoice = document.createElement('img');
+        gifNoice.src = 'https://media.tenor.com/pEtOxcxh_xUAAAAC/noice-michael-rosen.gif';
+        gifNoice.alt = 'gif-noice';
+        li.appendChild(gifNoice);
+    }else{
+        li.style.backgroundColor = 'red';
+        let gifDisappointed = document.createElement('img');
+        gifDisappointed.src = 'https://i.gifer.com/2Bz.gif';
+        gifDisappointed.alt = 'gif-disappointed';
+        li.appendChild(gifDisappointed);
+    }
+    i++;
 });
