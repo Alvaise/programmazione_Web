9c58406c62681248881d5da4dab7e83afb53c41d
Alvaise <79141448+Alvaise@users.noreply.github.com>
2023-05-27 19:15:25 +0200

    aa

--- js-10-events-and-animation/02-calculate/main.js
+++ js-10-events-and-animation/02-calculate/main.js
@@ -9,3 +9,48 @@
  * Afterwards, change the code so that you respond to key presses so that the
  * user doesn't have to click the button
  */
+
+/**
+ * function that give the square of a given number
+ * @param {*} n 
+ */
+function squareNumber(n) {
+    let nSq = Math.pow(n,2);
+    console.log('The result of squaring the number '+n+' is '+nSq);
+    return nSq;
+}
+/**
+ * function that give the half of a given number
+ * @param {*} n 
+ */
+function halfNumber(n) {
+    hN = n/2;
+    console.log('Half of '+n+' is '+hN);
+    return hN;
+}
+/**
+ * percent of the first number in the second
+ * @param {*} num1 
+ * @param {*} num2 
+ * @returns 
+ */
+function percentOf(num1,num2) {
+    let numP= (num1/num2)*100
+    console.log(num1+' is '+numP+'% of '+num2);
+    return numP; 
+}
+/**
+ * function to calculate the area of circle given the radius
+ * @param {*} radius 
+ * @returns 
+ */
+function areaOfCircle(radius) {
+    let area = Math.PI * (Math.pow(radius,2));
+    console.log('The area for a circle with radius '+ radius+ ' is '+ area.toFixed(2));
+    return area.toFixed(2);
+}
+/**
+ * function that execute the functions halfNumber(), squareNumber(), areaOfCircle(), percentOF()
+ * @param {*} n 
+ * @returns empty string
+ */
